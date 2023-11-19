d5ad854b12fd15fb1d3d9f525da463160a80ebd3
Dawid Kowalski <91938265+DKowalski111@users.noreply.github.com>
2023-11-19 22:24:40 +0100

    Delete repository directory

--- repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package taskmanager.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import taskmanager.project.entity.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
